62d6a2b760b67d254937353d3d51ed5e948fdacf
朱修相 <zxx960@qq.com>
2024-06-07 17:10:52 +0800

    feat: 第二版本

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="container" style="width: 800px; margin: auto">
-    <div style="text-align: center">
+  <div
+    class="container"
+    style="width: 800px; margin: auto; display: flex; flex-direction: column"
+  >
+    <div style="text-align: center; margin-top: 100px">
       <div class="title">无聊小姐姐</div>
-      <div class="title">无聊就来刷小姐姐吧</div>
-      <vue3videoPlay v-bind="options" />
+      <div class="subtitle">无聊就来刷小姐姐吧</div>
+      <video
+        :src="options.src"
+        style="margin-top: 30px"
+        width="800"
+        height="450"
+        controls
+        autoplay
+        muted
+      ></video>
       <div class="buttons">
         <button @click="prev">上一个</button>
         <button @click="next">下一个</button>
@@ -13,41 +24,14 @@
 </template>
 
 <script>
-import "vue3-video-play/dist/style.css";
-import vue3videoPlay from "vue3-video-play"; // 引入组件
 export default {
-  components: {
-    vue3videoPlay,
-  },
+  components: {},
   data() {
     return {
       urlList: [],
       index: 0,
       options: {
-        width: "800px", //播放器高度
-        height: "450px", //播放器高度
-        color: "#409eff", //主题色
-        title: "", //视频名称
-        src: " ", //视频源
-        muted: false, //静音
-        webFullScreen: false,
-        speedRate: ["0.75", "1.0", "1.25", "1.5", "2.0"], //播放倍速
-        autoPlay: true, //自动播放
-        loop: false, //循环播放
-        mirror: false, //镜像画面
-        ligthOff: false, //关灯模式
-        volume: 0.3, //默认音量大小
-        control: true, //是否显示控制
-        controlBtns: [
-          "audioTrack",
-          "quality",
-          "speedRate",
-          "volume",
-          "setting",
-          "pip",
-          "pageFullScreen",
-          "fullScreen",
-        ], //显示所有按钮,
+        src: "", //视频源
       },
     };
   },
@@ -66,17 +50,17 @@ export default {
       );
     },
     next() {
-      if (this.index < this.urlList.length-1) {
+      if (this.index < this.urlList.length - 1) {
         this.index++;
         this.options.src = this.urlList[this.index];
-        return
+        return;
       }
       fetch("https://api.pearktrue.cn/api/random/xjj/?type=json").then(
         (res) => {
           res.json().then((data) => {
             this.options.src = data.video;
             this.urlList.push(data.video);
-            this.index = this.urlList.length-1
+            this.index = this.urlList.length - 1;
           });
         }
       );
@@ -89,13 +73,20 @@ export default {
         this.options.src = this.urlList[this.index];
       }
     },
+    error(val) {
+      console.log(val);
+    },
   },
 };
 </script>
 <style scoped>
 .title {
-  font-size: 30px;
+  font-size: 50px;
   font-weight: bold;
   color: #000;
+}
+.subtitle{
+  font-size: 25px;
+  margin-top: 10px;
 }
 </style>
